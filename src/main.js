8e79646c00ea80aeda6143b45db381d074fc23d3
liuzx_emily <liuzx_emily@qq.com>
2025-03-06 10:23:44 +0800

    feat: enable msw only in dev environment

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import { worker } from "./mock";
 
 async function bootstrap() {
-  await worker.start(); // 等待 worker 启动完成
+  if (import.meta.env.DEV) {
+    await worker.start(); // 等待 worker 启动完成
+  }
   createApp(App).mount("#app"); // 再启动 Vue 应用
 }
 
